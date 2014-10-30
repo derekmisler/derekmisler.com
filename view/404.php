4fd8586c445185d88c1e56d49da03a885682920d
Derek Misler <derekmisler@gmail.com>
2014-10-30 14:42:28 -0400

    404 page needs custom meta tags

--- view/404.php
+++ view/404.php
@@ -1,4 +1,28 @@
-	<?php include(VIEW_INCLUDE_PATH.'sections/meta.inc.php'); ?>
+<!doctype html>
+<html lang="en-us" itemscope itemtype="http://schema.org/Article">
+	<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# product: http://ogp.me/ns/product#">
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<title>Where is Derek Misler?</title>
+		<meta name="description" content="This is a 404 page.">
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,300italic,700,700italic|Playfair+Display:700" rel="stylesheet" type="text/css">
+		<link href="<?= VIEW_PATH; ?>style.css" rel="stylesheet" />
+		<link rel="shortcut icon" href="<?= BASE; ?>favicon.ico" type="image/x-icon" />
+		<link rel="icon" href="<?= BASE; ?>favicon.ico" type="image/x-icon" />
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-17984733-3', 'auto');
+			ga('send', 'pageview');
+
+		</script>
+	</head>
 	<body data-spy="scroll" data-target=".navbar-collapse">
 
 		<div id="top" class="jumbotron" data-src="<?= $current_header['source']; ?>" data-position="<?= $current_header['position']; ?>">

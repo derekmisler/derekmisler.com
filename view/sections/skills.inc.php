bd1ff5a416d941ce5769329106cd94a16af5fc03
derekmisler <derekmisler@gmail.com>
2014-10-31 06:48:23 -0400

    fixing mobile stuff

--- view/sections/skills.inc.php
+++ view/sections/skills.inc.php
@@ -7,7 +7,7 @@
 	<?php if(count($skills) > 0) { ?>
 	
 	<div class="col-md-12">
-		<ul class="small-block-grid-1 medium-block-grid-2">
+		<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
 		
 		<?php foreach($skills as $index => $skill) { ?>
 		

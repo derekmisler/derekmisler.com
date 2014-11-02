672a994c78228f5c85dfc603ce5c0fecae4bb1ec
derekmisler <derekmisler@gmail.com>
2014-11-02 18:31:52 -0500

    hid tools

--- view/sections/skills.inc.php
+++ view/sections/skills.inc.php
@@ -41,15 +41,15 @@
 	<?php } ?>
 	
 </div>
-
-<hr />
-
-<h3>Tools</h3>
-
-<div class="row">
-
 	<?php if(count($tools) > 0) { ?>
 	
+
+	<hr />
+
+	<h3>Tools</h3>
+
+	<div class="row">
+
 	<div class="col-md-12">
 		<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
 		

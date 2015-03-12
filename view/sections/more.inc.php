ee8ea351b2a0c0af02de411cc9d67ecceedac31c
derekmisler <derekmisler@gmail.com>
2015-03-12 05:57:00 -0400

    removed stuff that isn't relevant, added c languages

--- view/sections/more.inc.php
+++ view/sections/more.inc.php
@@ -3,25 +3,15 @@
 
 <div class="row">
 	<div class="col-md-12">
-		<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-6 text-center">
+		<ul class="small-block-grid-2 medium-block-grid-4 text-center">
 			<li>
 				<a href="mailto:<?= $contact->email; ?>" target="_blank">
 					<span class="icon-mail"></span>
 				</a>
 			</li>
 			<li>
-				<a href="tel:<?= $contact->phone; ?>">
-					<span class="icon-phone"></span>
-				</a>
-			</li>
-			<li>
 				<a href="<?= $contact->linkedin; ?>" target="_blank">
 					<span class="icon-linkedin"></span>
-				</a>
-			</li>
-			<li>
-				<a href="<?= $contact->twitter; ?>" target="_blank">
-					<span class="icon-twitter"></span>
 				</a>
 			</li>
 			<li>

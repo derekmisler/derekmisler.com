20878d9c3602d3ab2eb6fdab022a3387e5b8a894
Derek Misler <derekmisler@gmail.com>
2015-07-13 17:01:21 -0400

    added my name to the title tags

--- view/sections/projects.inc.php
+++ view/sections/projects.inc.php
@@ -10,15 +10,15 @@
 	
 	<div class="col-sm-6 col-xs-12">
 		<figure class="effect">
-			<img src="<?= VIEW_PATH; ?>images/loader.gif" data-src="<?= VIEW_PATH.'images/'.$project->image; ?>" data-src-retina="<?= VIEW_PATH.'images/retina/'.$project->image; ?>" alt="<?= $project->title; ?>" />
+			<img src="<?= VIEW_PATH; ?>images/loader.gif" data-src="<?= VIEW_PATH.'images/'.$project->image; ?>" data-src-retina="<?= VIEW_PATH.'images/retina/'.$project->image; ?>" alt="<?= $project->title; ?> by <?= $profile->full_name; ?>" />
 			<noscript>
-				<img src="<?= VIEW_PATH.'images/'.$project->image; ?>" alt="<?= $project->title; ?>" />
+				<img src="<?= VIEW_PATH.'images/'.$project->image; ?>" alt="<?= $project->title; ?> by <?= $profile->full_name; ?>" />
 			</noscript>
 			<figcaption>
 				<h3><?= $project->title; ?></h3>
 				<p><?= $project->description; ?></p>
 				<p><strong>Skills Utilized:</strong> <br /><?= $project->tags; ?></p>
-				<a href="<?= $project->link; ?>" target="_blank" title="View Project">View more</a>
+				<a href="<?= $project->link; ?>" target="_blank" title="View <?= $project->title; ?> by <?= $profile->full_name; ?>">View more</a>
 				<span class="icon">
 					<span class="icon-new-window"></span>
 				</span>

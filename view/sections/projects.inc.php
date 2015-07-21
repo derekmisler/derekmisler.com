94942f0d86441b52c598591b85feb15b1a95c829
derekmisler <derekmisler@gmail.com>
2015-07-21 08:06:45 -0400

    new background colors

--- view/sections/projects.inc.php
+++ view/sections/projects.inc.php
@@ -1,13 +1,8 @@
-<h2><?= $profile->full_name; ?>'s Favorite Projects</h2>
-
-<hr />
-
 <?php if(count($projects) > 0) { ?>
-
+<h2><?= $profile->full_name; ?>'s Favorite Projects</h2>
+<hr />
 <div class="row">
-
 	<?php foreach($projects as $index => $project) { ?>
-
 	<div class="col-sm-6 col-xs-12">
 		<figure class="effect">
 			<img src="<?= VIEW_PATH; ?>images/loader.gif" data-src="<?= VIEW_PATH.'images/'.$project->image; ?>" data-src-retina="<?= VIEW_PATH.'images/retina/'.$project->image; ?>" alt="<?= $project->title; ?> by <?= $profile->full_name; ?>" />
@@ -25,10 +20,7 @@
 			</figcaption>
 		</figure>
 	</div>
-
 	<?php } ?>
-
 </div>
-
 <?php } else { ?>
 <?php } ?>
--- view/sections/skills.inc.php
+++ view/sections/skills.inc.php
@@ -1,120 +1,66 @@
 <h2><?= $profile->full_name; ?>'s Qualifications</h2>
-
 <hr />
-
 <h3>Independent Learning</h3>
-
 <?php if(count($coursework) > 0) { ?>
-
 <div class="coursework">
-
-
 	<div class="coursework row">
-
 		<div class="col-md-12">
 			<ul class="small-block-grid-1 medium-block-grid-2">
-
-			<?php foreach($coursework as $index => $courses) { ?>
-
+				<?php foreach($coursework as $index => $courses) { ?>
 				<li>
 					<span class="course-title"><?= $courses->title; ?></span>
 					<h4 class="course-specification"><?= $courses->specification; ?></h4>
 				</li>
-
-			<?php } ?>
-
+				<?php } ?>
 			</ul>
 		</div>
 	</div>
-
-
 </div>
-
 <?php } else { ?>
-
-
 <?php } ?>
-
+<?php if(count($skills) > 0) { ?>
 <hr />
-
 <h3>Proficiencies</h3>
-
 <div class="row">
-
-	<?php if(count($skills) > 0) { ?>
-
 	<div class="col-md-12">
 		<ul class="small-block-grid-1 medium-block-grid-2">
-
 		<?php foreach($skills as $index => $skill) { ?>
-
 			<li>
 				<span class="ability-title"><?= $skill->title; ?></span>
 				<span class="ability-score">
-
 				<?php for($stars = 1; $stars <= 5; $stars++) { ?>
-
 					<span class="icon icon-star <?= ($skill->level >= $stars) ? 'filled' : ''; ?>"></span>
-
 				<?php } ?>
-
 				</span>
 			</li>
-
 			<?php if(ceil(count($skills) / 2) == $index + 1) { ?>
-
 			<?php } ?>
-
 		<?php } ?>
-
 		</ul>
 	</div>
-
-	<?php } else { ?>
-
-
-	<?php } ?>
-
 </div>
-	<?php if(count($tools) > 0) { ?>
-
-
-	<hr />
-
-	<h3>Tools</h3>
-
-	<div class="row">
-
+<?php } else { ?>
+<?php } ?>
+<?php if(count($tools) > 0) { ?>
+<hr />
+<h3>Tools</h3>
+<div class="row">
 	<div class="col-md-12">
 		<ul class="small-block-grid-1 medium-block-grid-2">
-
 		<?php foreach($tools as $index => $tool) { ?>
-
 			<li>
 				<span class="ability-title"><?= $tool->title; ?></span>
 				<span class="ability-score">
-
 				<?php for($stars = 1; $stars <= 5; $stars++) { ?>
-
 					<span class="icon icon-star <?= ($tool->level >= $stars) ? 'filled' : ''; ?>"></span>
-
 				<?php } ?>
-
 				</span>
 			</li>
-
 			<?php if(ceil(count($tools) / 2) == $index + 1) { ?>
-
 			<?php } ?>
-
 		<?php } ?>
-
 		</ul>
 	</div>
-
-	<?php } else { ?>
-
-
-	<?php } ?>
-
 </div>
+<?php } else { ?>
+<?php } ?>

71e5c6d57c1143d1e201e53b4b4a9bebad782c1f
derekmisler <derekmisler@gmail.com>
2015-11-21 11:35:26 -0500

    added featherlight for displaying print work

--- view/resume.php
+++ view/resume.php
@@ -59,7 +59,7 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="<?= VIEW_PATH; ?>js/script.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
 
 	</body>
 </html>

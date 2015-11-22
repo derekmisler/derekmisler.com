0f8bd1f243f52f854a7cb6096b22f8e7c2c1ea06
derekmisler <derekmisler@gmail.com>
2015-11-22 10:16:12 -0500

    updated a project

--- view/sections/profile.inc.php
+++ view/sections/profile.inc.php
@@ -30,20 +30,15 @@
 				<a title="View <?= $profile->full_name; ?>'s LinkedIn profile" href="<?= $contact->linkedin; ?>" target="_blank"><span class="icon-linkedin"></span>&nbsp;LinkedIn</a>
 			</li>
 			<li>
-				<a href="https://github.com/derekmisler/derekmisler.com" target="_blank"><span class="icon-new-window"></span>&nbsp;View this site on GitHub.</a>
-			</li>
-			<li>
 				<a title="View <?= $profile->full_name; ?>'s GitHub profile" href="<?= $contact->github; ?>" target="_blank"><span class="icon-github"></span>&nbsp;GitHub</a>
 			</li>
 			<li>
 				<a title="View <?= $profile->full_name; ?>'s StackOverflow profile" href="<?= $contact->stackoverflow; ?>" target="_blank"><span class="icon-stackoverflow"></span>&nbsp;StackOverflow</a>
 			</li>
 			<li>
-				<a title="View <?= $profile->full_name; ?>'s Twitter" href="<?= $contact->twitter; ?>" target="_blank"><span class="icon-twitter"></span>&nbsp;Twitter</a>
-			</li>
-			<li>
 				<a title="View <?= $profile->full_name; ?>'s Flickr profile" href="<?= $contact->flickr; ?>" target="_blank"><span class="icon-flickr"></span>&nbsp;Flickr</a>
 			</li>
 		</ul>
+		<h6>This site was built with PHP, HTML5, and Less. <a href="https://github.com/derekmisler/derekmisler.com" target="_blank">Learn more on GitHub.&nbsp;<small><span class="icon-new-window"></span></small></a></h6>
 	</div>
 </div>

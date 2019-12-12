b1b77a692e864f0df236dfbb74a220f132d64e2d
derekmisler <derekmisler@gmail.com>
2019-12-12 14:44:22 -0500

    hr

--- components/Hr.tsx
+++ components/Hr.tsx
@@ -1,12 +1,18 @@
-import styled from 'styled-components'
+import { memo, FC, useContext } from 'react'
+import styled, { ThemeContext } from 'styled-components'
 import { LAYOUT_DEFAULTS } from 'styles'
 
-const { borderColor, borderSize, spacing } = LAYOUT_DEFAULTS
+const { borderSize, spacing } = LAYOUT_DEFAULTS
 
-export const Hr = styled.hr`
-  background-color: ${borderColor};
+const StyledHr = styled.hr<{ borderColor: string }>`
+  background-color: ${({ borderColor }) => borderColor};
   border: 0;
-  color: ${borderColor};
+  color: ${({ borderColor }) => borderColor};
   height: ${borderSize};
   margin: ${spacing.medium} auto;
 `
+
+export const Hr: FC<{}> = memo(() => {
+  const { border } = useContext(ThemeContext)
+  return <StyledHr borderColor={border} />
+})

54bd0cd3db7d97cacc978f75ea8bc4072aa2f3dd
Derek Misler <derekmisler@gmail.com>
2020-01-24 19:01:39 -0500

    splash screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,15 +24,27 @@ class MainApp extends App {
           <link rel='manifest' href='/site.webmanifest' />
 
           {/* Chrome for Android */}
-          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='mobile-web-app-capable' content='yes' />
           <meta name='theme-color' content={themes.dark.background} />
           <meta name='application-name' content={profile.fullName} />
           <link rel='icon' sizes='192x192' href='/icon-192.png' />
 
           {/* Safari for iOS */}
+          <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-title' content={profile.fullName} />
           <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
           <link rel='apple-touch-icon' href='/icon-192.png' />
+          <link href='splashscreens/iphone5_splash.png' media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/iphone6_splash.png' media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/iphoneplus_splash.png' media='(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/iphonex_splash.png' media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/iphonexr_splash.png' media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/iphonexsmax_splash.png' media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/ipad_splash.png' media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/ipadpro1_splash.png' media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/ipadpro3_splash.png' media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+          <link href='splashscreens/ipadpro2_splash.png' media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)' rel='apple-touch-startup-image' />
+
 
           {/* Win8 */}
           <meta name='msapplication-TileColor' content={themes.dark.background} />

591344fc54dc5d4dde39f4490c720c8de4db3e41
Derek Misler <derekmisler@users.noreply.github.com>
2020-01-25 17:05:45 -0500

    linting and lighthouse in ci (#3)
    * Create tests.yml
    
    * forgot npm i
    
    * npm ci instead
    
    * expected single quotes
    
    * lighthouse tokens and whatnot
    
    * is it this path
    
    * do not need that
    
    * is this right
    
    * key in next config
    
    * trying this path now
    
    * token, maybe
    
    * autorun script
    
    * autorun
    
    * is THIS where it goes
    
    * testing stuff
    
    * gotta get that  perfect score
    
    * upload
    
    * token again
    
    * separate tasks
    
    * run on url
    
    * not https
    
    * i really do not care about this
    
    * deleted

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const urlsToCache = [
   'https://use.typekit.net/jzw6tzs.css'
 ]
 const nextConfig = {
+  env: {
+    LHCI_GITHUB_APP_TOKEN: process.env.LHCI_GITHUB_APP_TOKEN,
+  },
   target: 'serverless',
   exportTrailingSlash: true,
   poweredByHeader: false,
--- styles/global.tsx
+++ styles/global.tsx
@@ -17,15 +17,15 @@ interface GlobalStyleProps {
 
 export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
   @font-face {
-    font-family: "neue-haas-grotesk-display";
-    src: url("https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/l?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3") format("woff2"), url("https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/d?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3") format("woff"), url("https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/a?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3") format("opentype");
+    font-family: 'neue-haas-grotesk-display';
+    src: url('https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/l?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3') format('woff2'), url('https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/d?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3') format('woff'), url('https://use.typekit.net/af/8a200c/00000000000000003b9b204a/27/a?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n6&v=3') format('opentype');
     font-style: normal;
     font-weight: 600;
     font-display: swap;
   }
   @font-face {
-    font-family: "neue-haas-grotesk-text";
-    src: url("https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/l?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3") format("woff2"), url("https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/d?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3") format("woff"), url("https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/a?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3") format("opentype");
+    font-family: 'neue-haas-grotesk-text';
+    src: url('https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/l?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3') format('woff2'), url('https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/d?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3') format('woff'), url('https://use.typekit.net/af/550c82/00000000000000003b9b2052/27/a?primer=7fa3915bdafdf03041871920a205bef951d72bf64dd4c4460fb992e3ecc3a862&fvd=n5&v=3') format('opentype');
     font-style: normal;
     font-weight: 500;
     font-display: swap;

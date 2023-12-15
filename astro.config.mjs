3cf86972589b2e36b90b399e83d36dfabf9b7b41
Derek Misler <derekmisler@gmail.com>
2023-12-15 15:15:50 -0500

    disabled analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [tailwind()]
 });

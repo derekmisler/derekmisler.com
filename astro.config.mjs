99466b00a435987d5e7cd6cfff72bfc511d17af4
Derek Misler <derekmisler@gmail.com>
2024-02-16 10:55:28 -0500

    robots.txt stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import vercelStatic from '@astrojs/vercel/static';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
   site: 'https://derekmisler.com',
+  integrations: [sitemap()],
 });

0188d49fde9ea4b6cdbaed7305b64e34f6ea82a3
Derek Misler <derekmisler@gmail.com>
2024-08-10 17:44:15 -0400

    i do not need this

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   site: 'https://derekmisler.com',
   integrations: [sitemap()],
 });
